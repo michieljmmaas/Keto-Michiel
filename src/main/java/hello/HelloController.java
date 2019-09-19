f28fab3a13c3d9e346aa9068d3a9ad264a78f324
Michiel Maas <michieljmmaas@gmail.com>
2019-09-19 16:51:36 +0200

    test met nieuew link

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+    	System.out.println("Hello");
+        return "Greetings from Spring Boot!";
+    }
+
+}
