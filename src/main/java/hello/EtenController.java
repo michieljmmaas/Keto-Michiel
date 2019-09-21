fbbe6edbaaa1649e7d142bd8c12206ab3e7a7460
Michiel Maas <michieljmmaas@gmail.com>
2019-09-21 20:55:32 +0200

    kan dingen verwijderen in gerrechten div

--- src/main/java/hello/EtenController.java
+++ src/main/java/hello/EtenController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +15,23 @@ public class EtenController {
 	@Autowired
 	EtenRepository AWEtenRepository;
 
+	@Autowired
+	GerechtRepository AWGerechtRepository;
+
 	@DeleteMapping(value = "/{id}")
 	public void deleteID(@PathVariable int id) {
 		Eten toDelete = AWEtenRepository.findById(id);
 		AWEtenRepository.delete(toDelete);
+	}
+
+	@DeleteMapping(value = "DeleteIngredient/{dishID}/{ingredientID}")
+	public void deleteIngredient(@PathVariable int dishID, @PathVariable int ingredientID) {
+		Eten toDelete = AWEtenRepository.findById(ingredientID);
+		Gerecht gerrecht = AWGerechtRepository.findById(dishID);
+		Set<Eten> ingredienten = gerrecht.getIngredienten();
+		ingredienten.remove(toDelete);
+
+		AWGerechtRepository.save(gerrecht);
 	}
 
 }
