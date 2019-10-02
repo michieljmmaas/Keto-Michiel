06aaaa565451da81ceffcb7ed1b469d7709e04ad
Michiel Maas <michieljmmaas@gmail.com>
2019-10-02 14:10:43 +0200

    opens browser

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,10 @@
 package hello;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +13,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	    Browse("http://localhost:8080/1");
+	}
+
+	public static void Browse(String url) {
+		if (Desktop.isDesktopSupported()) {
+			Desktop desktop = Desktop.getDesktop();
+			try {
+				desktop.browse(new URI(url));
+			} catch (IOException | URISyntaxException e) {
+				e.printStackTrace();
+			}
+		} else {
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
